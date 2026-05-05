19c1f5e97c1e2b56792479bba8adb165946e16db
Lidia Rodrigues <rodriguesferreira2612@gmail.com>
2026-05-05 08:12:34 -0300

    atualizacao de aulas 5.5.26

--- Aula 7/sete.py
+++ Aula 7/sete.py
@@ -1,1 +1,31 @@
 # Projeto cancela Automatica
+
+# Projeto 1:
+# Projeto: Precisamos de um algoritmo para gerenciamento de cancelas para um shopping.
+# Toda entrada e saída irá ser sinalizada
+# Valores para entrada e permanência do veículo deverá ser pergutado
+# As entrada deverão ser registradas por placa.
+
+Passo 1:  
+Perguntar informações sobre o veiculo ou forma acesso
+Pressionar o botao para emitir ticket
+Verificar se possui TAG para acesso liberado
+Se possuir erros informar ao usuário
+
+# Passo 2:
+# Verificar tempo de permanência
+# Valor a ser cobrado
+
+# Passo 3:
+# Saída como será?
+# Calcular tempo de permanência
+# Se for TAG gerar na fatura da TAG
+# Pagar ticket
+# Devolver ticket na saída
+
+# Passo 4:
+# Gerar relatório de entradas e saídas
+# Tratamento de Erros
+# Revisão do código
+
+Passo 1:
